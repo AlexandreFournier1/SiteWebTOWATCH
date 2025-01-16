29ffd1a548a9c6bdc568797fe9cb260b2bc0c6d0
alexlefou <alexandre.fournier@student.hepl.be>
2025-01-16 12:03:26 +0100

    :lock: Pour la sécurité

--- backup/script.js
+++ backup/script.js
@@ -97,10 +97,18 @@ function generateSubcategories(film) {
                         ${item.title}
                     </p>
                     <ul id="${item.id}-dropdown" class="dropdown-menu hidden">
-                        <li style="list-style: none;"><a href="${item.lien_info}" target="_blank" style="background-color: #00DD00">INFORMATIONS</a></li>
-                        <li style="list-style: none;"><a href="${item.lien_trailer}" target="_blank" style="background-color: #00DD00">TRAILER</a></li>
-                        <li style="list-style: none;"><a href="${item.lien_vo}" target="_blank" style="background-color: #00DD00">FULL ANGLAIS</a></li>
-                        <li style="list-style: none;"><a href="${item.lien_vf}" target="_blank" style="background-color: #00DD00">FULL FRANCAIS</a></li>
+                        <li style="list-style: none;" style="background-color: #00DD00">
+                            <a href="${item.lien_info}" target="_blank" rel="noopener noreferrer" style="background-color: #00DD00">INFORMATIONS</a>
+                        </li>
+                        <li style="list-style: none;" style="background-color: #00DD00">
+                            <a href="${item.lien_trailer}" target="_blank" rel="noopener noreferrer" style="background-color: #00DD00">TRAILER</a>
+                        </li>
+                        <li style="list-style: none;" style="background-color: #00DD00">
+                            <a href="${item.lien_vo}" target="_blank" rel="noopener noreferrer" style="background-color: #00DD00">FULL ANGLAIS</a>
+                        </li>
+                        <li style="list-style: none;" style="background-color: #00DD00">
+                            <a href="${item.lien_vf}" target="_blank" rel="noopener noreferrer" style="background-color: #00DD00">FULL FRANCAIS</a>
+                        </li>
                     </ul>
                 </div>
             </li>
@@ -113,10 +121,18 @@ function generateSubcategories(film) {
                         ${film.title}
                     </p>
                     <ul id="${film.id}-dropdown" class="dropdown-menu hidden">
-                        <li style="list-style: none;"><a href="${film.lien_info}" target="_blank" style="background-color: #00DD00">INFORMATIONS</a></li>
-                        <li style="list-style: none;"><a href="${film.lien_trailer}" target="_blank" style="background-color: #00DD00">TRAILER</a></li>
-                        <li style="list-style: none;"><a href="${film.lien_vo}" target="_blank" style="background-color: #00DD00">FULL ANGLAIS</a></li>
-                        <li style="list-style: none;"><a href="${film.lien_vf}" target="_blank" style="background-color: #00DD00">FULL FRANCAIS</a></li>
+                        <li style="list-style: none;" style="background-color: #00DD00">
+                            <a href="${film.lien_info}" target="_blank" rel="noopener noreferrer" style="background-color: #00DD00">INFORMATIONS</a>
+                        </li>
+                        <li style="list-style: none;" style="background-color: #00DD00">
+                            <a href="${film.lien_trailer}" target="_blank" rel="noopener noreferrer" style="background-color: #00DD00">TRAILER</a>
+                        </li>
+                        <li style="list-style: none;" style="background-color: #00DD00">
+                            <a href="${film.lien_vo}" target="_blank" rel="noopener noreferrer" style="background-color: #00DD00">FULL ANGLAIS</a>
+                        </li>
+                        <li style="list-style: none;" style="background-color: #00DD00">
+                            <a href="${film.lien_vf}" target="_blank" rel="noopener noreferrer" style="background-color: #00DD00">FULL FRANCAIS</a>
+                        </li>
                     </ul>
                 </div>
             </li>
